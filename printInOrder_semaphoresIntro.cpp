eec919a02073a359f684d4099a45655da8948a6c
Ketaki Joshi <joshiketakir@gmail.com>
2023-12-25 15:53:20 -0800

    Update printInOrder_semaphoresIntro.cpp

--- printInOrder_semaphoresIntro.cpp
+++ printInOrder_semaphoresIntro.cpp
@@ -44,6 +44,7 @@ Constraints:
 // why use semaphore here:
 // here we are signalling that after a strict task ordering, the earlier one tells the next one to start after it has finished
 // here we are not performing the action for all available objects at the next possible opening
+// we can convey a sense of ordering via the semaphore. mutex lock is a lock for all threads....so how can you convey sense of ordering
 
 class Foo {
 public:
