3e322e331ca574c92e9fc458dffa8b3e8eb81779
Ketaki Joshi <joshiketakir@gmail.com>
2024-01-14 17:17:04 -0800

    Update longestPalindrome.cpp

--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -46,8 +46,79 @@ public:
         cout << "\n substring returning" << subString;
         return subString;
     }
+   string longestPalindrome(string s) {
+        int len = s.length();
+        assert(len >= 1 && len <= 1000);
+        // think of current charachter as center and expand to eft and roght
+        // while checkign the palindrome
 
-    string longestPalindrome(string s) {
+        int maxLen = 0;
+        string longestPal = "";
+        int l, r;
+
+        for(int i = 0; i <len; i++)
+        {
+            // odd length
+            // start from current charachter as center and move in both directions
+            l = i; r= i;
+            while(l >=0 && r < len && s[l] == s[r])
+            {
+                if(r-l+1 > maxLen){
+                    maxLen = r-l+1;
+                    longestPal = s.substr(l, r-l+1);
+                }
+                l--;
+                r++;
+            }
+
+            // even length
+            //start from central two charachters and move in both directions
+            l = i; r = i+1;
+            while(l >=0 && r < len && s[l]==s[r])
+            {
+                if(r-l+1 > maxLen){
+                    maxLen = r-l+1;
+                    longestPal = s.substr(l, r-l+1);
+                }
+                l--;
+                r++;
+            }
+        }
+        return longestPal;
+        /*int resLen = 0;
+        string res;
+        int l, r;
+
+
+        for(int i = 0; i < len; i++)
+        {
+            // odd length pal string
+            l = i; r = i;
+            while(l >=0 && r < len && s[l] == s[r])
+            {
+                if(resLen < (r-l+1)){
+                    res = s.substr(l, r-l+1);
+                    resLen = r-l+1;
+                }
+                l--;
+                r++;
+            }
+            // even length pal string
+            l = i; r = i+1;
+            while(l >=0 && r < len && s[l] == s[r])
+            {
+                if(resLen < r-l+1){
+                    res = s.substr(l, r-l+1);
+                    resLen = r-l+1;
+                }
+                l--;
+                r++;
+            }
+        }
+        return res;*/
+    }
+};
+    /*string longestPalindrome(string s) {
         int len = s.length();
         assert(len >= 1 && len <= 1000);
         int resLen = 0;
@@ -81,5 +152,5 @@ public:
             }
         }
         return res;
-    }
+    }*/
 };
