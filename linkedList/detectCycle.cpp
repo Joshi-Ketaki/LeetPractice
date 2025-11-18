9bed518a65f646fc0323c9fd0866506f679c7630
Ketaki Joshi <joshiketakir@gmail.com>
2025-11-18 07:53:21 -0500

    Add cycle detection method for linked list
    Implement cycle detection in a linked list using the fast and slow pointer technique.

---
+++ linkedList/detectCycle.cpp
@@ -0,0 +1,43 @@
+/*
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+*/
+
+// Intuition: explained in code
+// TC: O(n) SC: O(1)
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        // use fast and slow ptr
+        // if a cycle exists, the fast ptr will eventually
+        // catch upto slow
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+};
