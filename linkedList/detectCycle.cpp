77d2d46aa97aa7b8768fcd16b0007f504941fd9f
Ketaki Joshi <joshiketakir@gmail.com>
2025-11-18 07:55:51 -0500

    Refine comments for detectCycle function
    Updated the comments to clarify the problem statement and examples for detecting a cycle in a linked list.

--- linkedList/detectCycle.cpp
+++ linkedList/detectCycle.cpp
@@ -1,11 +1,38 @@
 /*
-Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
-Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+Return true if there is a cycle in the linked list. Otherwise, return false.
 
-Note: index is not given to you as a parameter.
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
 */
 
 // Intuition: explained in code
